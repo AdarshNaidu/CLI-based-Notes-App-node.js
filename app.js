587129d2dd2a449da1f770524bb1e75e603d5aec
AdarshNaidu <naidu.adarsh2511@gmail.com>
2019-12-14 13:57:26 +0530

    argument parsing with yargs npm library

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
 const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require("./notes.js");
 
-let notes = getNotes();
+// Customize yargs version number
+yargs.version('1.1.0');
 
-// let isEmail = validator.isEmail("naidu.adarsh@Speec.com");
+// add command
+yargs.command({
+    command: 'add',
+    description: 'add a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log(chalk.yellow("Title: ") + argv.title);
+        console.log(chalk.blue("Body: ") + argv.body);
+    }
+})
 
-let printIsEmail = function(email){
-    console.log(chalk.yellow(email) + " " + (validator.isEmail(email)?chalk.green("true") : chalk.red("false")));
-}
+// remove command
+yargs.command({
+    command: 'remove',
+    description: 'remove a note',
+    handler: function(argv){
+        console.log("remove a note");
+    }
+})
 
-printIsEmail("da.dasr@g.edu.com");
+// list command
+yargs.command({
+    command: 'list',
+    description: 'list all notes',
+    handler: function(){
+        console.log("list all the notes");
+    }
+})
+
+// read command
+yargs.command({
+    command: 'read',
+    description: 'read notes',
+    handler: function(){
+        console.log("read the notes");
+    }
+})
+
+yargs.parse();
+
